d5811bd48d2ff19ec4645f19aae917dc4cef3daf
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-08-20 17:44:32 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,43 +3,83 @@ Config = {}
 Config.RestrictedChannels = {
     [1] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [2] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [3] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [4] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [5] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [6] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [7] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [8] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [9] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     },
     [10] = {
         police = true,
-        ambulance = true
+        bcso = true,
+        sasp = true,
+        sapr = true,
+        ambulance = true,
+        firefighter = true
     }
 }
 
